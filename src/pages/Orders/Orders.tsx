452343d9e2dfef60dff0fe48545e61644102aadf
Christoffer <mech1803@gmail.com>
2025-08-25 22:18:23 +0200

    Remove AddGames component, add Orders page with order fetching from Supabase, and create associated styles

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styles from './Orders.module.css';
+import supabase from '../../../supabaseConfig';
 
 const Orders = () => {
-	return <div>Order</div>;
+	const [orders, setOrder] = useState(null);
+
+	useEffect(() => {
+		const fetchOrders = async () => {
+			let { data: user_orders, error } = await supabase
+				.from('user_orders')
+				.select('*')
+				.range(0, 9);
+			console.log(user_orders);
+
+			setOrder(user_orders);
+		};
+
+		fetchOrders();
+	}, []);
+
+	return (
+		<div className={styles.orders}>
+			<div className={styles.ordersContainer}>
+				<div className={styles.ordersHeader}>
+					<div>Order nr</div>
+					<div>Status</div>
+					<div>Order Status</div>
+				</div>
+				<ul className={styles.orderList}>
+					{orders &&
+						orders.map((item) => (
+							<li className={styles.orderItem} key={item.id}>
+								<div>
+									<p className={styles.order_number}>{item.order_number}</p>
+								</div>
+								<div>
+									<p className={styles.order_status}>{item.order_status}</p>
+								</div>
+								<div>
+									<p className={styles.created_at}>{item.created_at}</p>
+								</div>
+							</li>
+						))}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Orders;
